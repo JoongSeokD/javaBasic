d78d8584661a7d8dae7f077107bc6e2929c9d048
JoongSeokD <ljseokd@gmail.com>
2019-12-14 23:04:49 +0900

    정수타입을 실수 타입으로 캐스팅 할때 정밀도 손실이 일어날 수 있다.
    float은 가수 23비트를 가진다.
    int는 32비트를 가진다.
    num2의 값은 23비트로 표현 할 수 없기때문에 근사치로 변환된다.
    
    double의 가수는 52비트이기 때문에 정밀도 손실을 피할 수 있다.

--- javaBasic/src/variable/FromIntToFloat.java
+++ javaBasic/src/variable/FromIntToFloat.java
@@ -5,10 +5,20 @@ public class FromIntToFloat {
 		int num1 = 123456780;
 		int num2 = 123456780;
 		
+		int num4 = 123456780;
+		int num5 = 123456780;
+		
 		float num3 = num2;
 		num2 = (int) num3;
 		
+		double num6 = num4;
+		num5 = (int) num6;
+		
 		int result = num1 - num2;
+		
+		int result2 = num4 - num5;
+		
 		System.out.println(result);
+		System.out.println(result2);
 	}
 }
