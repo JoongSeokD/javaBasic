e9c5129fb057fe9054ff203f1cff4c7b7a523bad
JoongSeokD <ljseokd@gmail.com>
2019-12-15 09:11:28 +0900

    문자열 비교

---
+++ javaBasic/src/operator/StringEqualsExample.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class StringEqualsExample {
+	public static void main(String[] args) {
+		String strVar1 = "ȫ�浿";
+		String strVar2 = "ȫ�浿";
+		String strVar3 = new String("ȫ�浿");
+		
+		System.out.println(strVar1 == strVar2);
+		System.out.println(strVar1 == strVar3);
+		System.out.println();
+		System.out.println(strVar1.equals(strVar2));
+		System.out.println(strVar1.equals(strVar3));
+		
+	}
+}
