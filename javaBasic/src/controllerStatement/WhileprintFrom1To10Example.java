046d9631f944335107e565aba90ca00beed7880e
JoongSeokD <ljseokd@gmail.com>
2019-12-16 11:10:19 +0900

    while문을 활용해 1부터 10까지 출력

---
+++ javaBasic/src/controllerStatement/WhileprintFrom1To10Example.java
@@ -0,0 +1,11 @@
+package controllerStatement;
+
+public class WhileprintFrom1To10Example {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
