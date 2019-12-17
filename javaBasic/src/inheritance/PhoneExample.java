ee21911564bd3748945963e2dc8c7bc837435a36
JoongSeokD <ljseokd@gmail.com>
2019-12-17 14:41:04 +0900

    추상 클래스 사용

---
+++ javaBasic/src/inheritance/PhoneExample.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class PhoneExample {
+	public static void main(String[] args) {
+		SmartPhone smartPhone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+}
