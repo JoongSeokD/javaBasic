760527fb34b37eccd6b130f2d7f7435f28b66149
JoongSeokD <ljseokd@gmail.com>
2019-12-18 17:27:38 +0900

    제한된 파라미터 타입

---
+++ javaBasic/src/generic/BoundedTypeParameterExample.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class BoundedTypeParameterExample {
+	public static void main(String[] args) {
+		// String str = Util3.compare("a", "b"); 타입오류
+		
+		int result1 = Util3.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 = Util3.compare(4.5, 3);
+		System.out.println(result2);
+		
+	}
+}
