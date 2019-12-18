51320752c2c05898b88bd2514faafe7d2323f4f9
JoongSeokD <ljseokd@gmail.com>
2019-12-18 14:20:12 +0900

    두 스레드가 교대로 돌아가면서 작업하는 예제

---
+++ javaBasic/src/thread/DataBox.java
@@ -0,0 +1,34 @@
+package thread;
+
+public class DataBox {
+	private String data;
+
+	public synchronized String getData() {
+		if (this.data == null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+		}
+		
+		String returnValue = data;
+		System.out.println("ConsumerThread가 읽은 데이터 : " + returnValue);
+		data = null;
+		notify();
+		return returnValue;
+	}
+
+	public synchronized void setData(String data) {
+		if (this.data != null) {
+			try {
+				wait();
+			} catch (InterruptedException e) {}
+		}
+		
+		this.data = data;
+		System.out.println("ProducerThread가 읽은 데이터 : " + data);
+		data = null;
+		notify();
+	}
+	
+	
+}
