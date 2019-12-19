0bfa632f18f87167eff701d773be37902aed4311
JoongSeokD <ljseokd@gmail.com>
2019-12-19 19:34:51 +0900

    이름을 키로 점수를 값으로 저장하기

---
+++ javaBasic/src/collectionFramework/HashMapExample.java
@@ -0,0 +1,46 @@
+package collectionFramework;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapExample {
+	public static void main(String[] args) {
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put("È«±æµ¿", 85);
+		map.put("ÀÏÁö¸Å", 90);
+		map.put("°­°¨Âù", 80);
+		map.put("½ÉÃ»ÀÌ", 95);
+		System.out.println("ÃÑ Entry ¼ö : " + map.size());
+		
+		Set<String> keySet = map.keySet();
+		Iterator<String> keyIterator = keySet.iterator();
+		while (keyIterator.hasNext()) {
+			String key = keyIterator.next();
+			Integer value = map.get(key);
+			System.out.println("\t" + key + " : " + value);
+		}
+		System.out.println();
+		
+		map.remove("È«±æµ¿");
+		System.out.println("ÃÑ Entry ¼ö : " + map.size());
+		
+		Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+		Iterator<Map.Entry<String, Integer>> entryIterator = entrySet.iterator();
+		
+		while (entryIterator.hasNext()) {
+			Map.Entry<String, Integer> entry = entryIterator.next();
+			String key = entry.getKey();
+			Integer value = entry.getValue();
+			System.out.println("\t" + key + " : " + value);
+		}
+		System.out.println();
+		
+		map.clear();
+		System.out.println("ÃÑ Entry ¼ö : " + map.size());
+		
+	}
+}
