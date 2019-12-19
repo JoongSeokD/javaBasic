449bfdf0a17a3a99c0868ed805baf11aa5928875
JoongSeokD <ljseokd@gmail.com>
2019-12-19 17:38:01 +0900

    정적 및 인스턴스 메소드 참조

---
+++ javaBasic/src/lambdaExpressions/Calculator.java
@@ -0,0 +1,10 @@
+package lambdaExpressions;
+
+public class Calculator {
+	public static int staticMethod(int x, int y) {
+		return x + y;
+	}
+	public int instanceMethod(int x, int y) {
+		return x + y;
+	}
+}
