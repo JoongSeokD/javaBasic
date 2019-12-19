fec6e72fcc500338f9ec04bed51992cfc7e5f63f
JoongSeokD <ljseokd@gmail.com>
2019-12-19 12:42:10 +0900

    커밋 테스트

--- javaBasic/src/lambdaExpressions/MyFunctionalInterface.java
+++ javaBasic/src/lambdaExpressions/MyFunctionalInterface.java
@@ -3,12 +3,12 @@ package lambdaExpressions;
 @FunctionalInterface
 public interface MyFunctionalInterface {
 	
-	// 매개변수가 없는 람다식 메소드
-//	void method();
+//	 매개변수가 없는 람다식 메소드
+	void method();
 	
 	// 매개변수가 있는 람다식 메소드
 //	void method(int x);
 	
-	// 리턴값이 있는 람다식 메소드
-	int method(int x, int y);
+//	// 리턴값이 있는 람다식 메소드
+//	int method(int x, int y);
 }
--- javaBasic/src/lambdaExpressions/MyFunctionalInterfaceExample.java
+++ javaBasic/src/lambdaExpressions/MyFunctionalInterfaceExample.java
@@ -5,17 +5,17 @@ public class MyFunctionalInterfaceExample {
 		MyFunctionalInterface fi;
 		
 		// 매개변수가 없는 람다식
-//		fi = ()->{
-//			String str = "method call1";
-//			System.out.println(str);
-//		};
-//		fi.method();
-//
-//		fi = ()-> {System.out.println("method call2");};
-//		fi.method();
-//		
-//		fi = () -> System.out.println("method call3");
-//		fi.method();
+		fi = ()->{
+			String str = "method call1";
+			System.out.println(str);
+		};
+		fi.method();
+
+		fi = ()-> {System.out.println("method call2");};
+		fi.method();
+		
+		fi = () -> System.out.println("method call3");
+		fi.method();
 		
 
 		// 매개변수가 있는 람다식
@@ -32,20 +32,20 @@ public class MyFunctionalInterfaceExample {
 //		fi.method(1);
 		
 		// 리턴 값이 있는 람다식
-		fi = (x, y) ->{
-			int result = x + y;
-			return result;
-		};
-		System.out.println(fi.method(2, 5));
-		
-		fi = (x, y) ->{return x + y;};
-		System.out.println(fi.method(2, 5));
-		
-		fi = (x, y) -> x + y;
-		System.out.println(fi.method(2, 5));
-		
-		fi = (x,y) -> sum(x,y);
-		System.out.println(fi.method(2, 5));
+//		fi = (x, y) ->{
+//			int result = x + y;
+//			return result;
+//		};
+//		System.out.println(fi.method(2, 5));
+//		
+//		fi = (x, y) ->{return x + y;};
+//		System.out.println(fi.method(2, 5));
+//		
+//		fi = (x, y) -> x + y;
+//		System.out.println(fi.method(2, 5));
+//		
+//		fi = (x,y) -> sum(x,y);
+//		System.out.println(fi.method(2, 5));
 		
 	}
 	
