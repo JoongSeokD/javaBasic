bd270bebb03a4ad282d06b0cb769c8a79603df67
JoongSeokD <ljseokd@gmail.com>
2019-12-20 17:22:59 +0900

    reduce() 메소드

---
+++ javaBasic/src/stream/ReductionExample.java
@@ -0,0 +1,31 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReductionExample {
+	public static void main(String[] args) {
+		List<Student> studentList = Arrays.asList(
+			new Student("È«±æµ¿", 92),
+			new Student("ÀÏÁö¸Å", 95),
+			new Student("ÀÌ¸ù·æ", 88)
+		);
+		
+		int sum1 = studentList.stream()
+				.mapToInt(Student :: getScore)
+				.sum();
+		int sum2 = studentList.stream()
+				.map(Student :: getScore)
+				.reduce((a,b) -> a+b )
+				.get();
+		int sum3 = studentList.stream()
+				.map(Student :: getScore)
+				.reduce(0, (a, b) -> a+b);
+		
+		System.out.println("sum1 : " + sum1);
+		System.out.println("sum2 : " + sum2);
+		System.out.println("sum3 : " + sum3);
+				
+				
+	}
+}
