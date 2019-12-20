91d54e54ee36d2994d3da6570fbafd67ac2400fd
JoongSeokD <ljseokd@gmail.com>
2019-12-20 14:37:38 +0900

    Iterator와 Stream의 순차 처리

---
+++ javaBasic/src/stream/IteratorVsStreamExample.java
@@ -0,0 +1,24 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class IteratorVsStreamExample {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("È«±æµ¿", "ÀÏÁö¸Å", "¼ºÃáÇâ");
+		
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()) {
+			String name =  it.next();
+			System.out.println(name);
+		}
+		
+		System.out.println();
+		
+		Stream<String> stream = list.stream();
+		stream.forEach(name -> System.out.println(name));
+		
+	}
+}
