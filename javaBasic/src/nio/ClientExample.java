adab1515cd5f5fe095a14d2e056bcafc9d235294
JoongSeokD <ljseokd@gmail.com>
2019-12-23 19:31:33 +0900

    서버/클라이언트 데이터 통신

--- javaBasic/src/nio/ClientExample.java
+++ javaBasic/src/nio/ClientExample.java
@@ -1,7 +1,9 @@
 package nio;
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 public class ClientExample {
 	public static void main(String[] args) {
@@ -12,8 +14,31 @@ public class ClientExample {
 			System.out.println("연결 요청");
 			socketChannel.connect(new InetSocketAddress("localhost", 5001));
 			System.out.println("연결 성공");
+			
+			
+			ByteBuffer byteBuffer = null;
+			Charset charset = Charset.forName("UTF-8");
+			
+			byteBuffer = charset.encode("Hello Server");
+			socketChannel.write(byteBuffer);
+			System.out.println("데이터 보내기 성공");
+			
+			byteBuffer = byteBuffer.allocate(100);
+			int byteCount = socketChannel.read(byteBuffer);
+			byteBuffer.flip();
+			String message = charset.decode(byteBuffer).toString();
+			System.out.println("데이터 받기 성공 : " + message);
+			
 		} catch (Exception e) {
 			// TODO: handle exception
+		}
+		
+		if (socketChannel.isOpen()) {
+			try {
+				socketChannel.close();
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
 		}
 	}
 }
--- javaBasic/src/nio/ServerExample.java
+++ javaBasic/src/nio/ServerExample.java
@@ -1,29 +1,47 @@
 package nio;
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 
 public class ServerExample {
 	public static void main(String[] args) {
-		ServerSocketChannel channel = null;
+		ServerSocketChannel serverSocketChannel = null;
 		try {
-			channel = ServerSocketChannel.open();
-			channel.configureBlocking(true);
-			channel.bind(new InetSocketAddress(5001));
+			serverSocketChannel = ServerSocketChannel.open();
+			serverSocketChannel.configureBlocking(true);
+			serverSocketChannel.bind(new InetSocketAddress(5001));
 			while (true) {
 				System.out.println("연결기다림");
-				SocketChannel socketChannel = channel.accept();
+				SocketChannel socketChannel = serverSocketChannel.accept();
 				InetSocketAddress isa = (InetSocketAddress) socketChannel.getRemoteAddress();
 				System.out.println("연결 수락함 : " + isa.getHostName());
+				
+				
+				ByteBuffer byteBuffer = null;
+				Charset charset = Charset.forName("UTF-8");
+				
+				byteBuffer = byteBuffer.allocate(100);
+				int byteCount = socketChannel.read(byteBuffer);
+				byteBuffer.flip();
+				String message = charset.decode(byteBuffer).toString();
+				System.out.println("데이터 받기 성공 : " + message);
+						
+				
+				byteBuffer = charset.encode("Hello Client");
+				socketChannel.write(byteBuffer);
+				System.out.println("데이터 보내기 성공");
+				
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		
-		if (channel.isOpen()) {
+		if (serverSocketChannel.isOpen()) {
 			try {
-				channel.close();
+				serverSocketChannel.close();
 			} catch (Exception e) {
 				// TODO: handle exception
 			}
