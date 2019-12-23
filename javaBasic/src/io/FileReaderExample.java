66e7440a89cafe2180e5d87ddaf7647f9015d4c0
JoongSeokD <ljseokd@gmail.com>
2019-12-23 12:48:29 +0900

    텍스트 파일 읽기

---
+++ javaBasic/src/io/FileReaderExample.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.io.FileReader;
+
+public class FileReaderExample {
+	public static void main(String[] args) throws Exception {
+		FileReader fr = new FileReader("C:/Users/OWNER/git/javaBasic/javaBasic/src/io/FileReaderExample.java");
+		
+		
+		int readCharNo;
+		char[] cbuf = new char[100];
+		while ((readCharNo = fr.read(cbuf)) != -1) {
+			String data = new String(cbuf, 0, readCharNo);
+			System.out.print(data);
+		}
+		fr.close();
+		
+	}
+}
